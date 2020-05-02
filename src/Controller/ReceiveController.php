6a96f271abc28627fee1560401ef029d1bbf1df1
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 23:44:41 +0200

    Add base for getting resources

--- src/Controller/ReceiveController.php
+++ src/Controller/ReceiveController.php
@@ -10,15 +10,43 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ReceiveController extends AbstractController
 {
+
+
+    /**
+     * @Route("/sso-base", name="sso_base")
+     * @return Response
+     */
+    public function actionThatRedirectsToLogin()
+    {
+        return $this->render('redirect.html.twig');
+    }
+
+    /**
+     * @Route("/sso-request", name="sso_request")
+     */
+    public function startRedirecting()
+    {// action
+    }
+
     /**
      * Catch SSO success message
      *
-     * @Route("/sso-success/{$token}")
+     * @Route("/sso-success/{token}", name="sso_receive")
+     *
      * @param string $token
      * @return Response
      */
     public function receive(string $token): Response
     {
         return $this->render('received.html.twig', ['token' => $token]);
+    }
+
+    /**
+     * @Route("/success")
+     * @return Response
+     */
+    public function success()
+    {
+        return $this->render('success.html.twig');
     }
 }
