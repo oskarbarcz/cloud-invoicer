43f1325c41a2b2355d489dc07b7957b570a5e97a
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-02 21:41:14 +0200

    Add base project setup

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
