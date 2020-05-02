da1ed1f7a5a78b35ac37536cdc5e20edbb810eb5
Oskar Barcz <kontakt@archi-tektur.pl>
2020-05-03 01:05:46 +0200

    Add complete consumer setup

--- src/Controller/ReceiveController.php
+++ src/Controller/ReceiveController.php
@@ -4,12 +4,31 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Account;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+use function dd;
 
 class ReceiveController extends AbstractController
 {
+    public const SSO_IP = '192.168.42.216:8000';
+
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
 
 
     /**
@@ -23,30 +42,77 @@ class ReceiveController extends AbstractController
 
     /**
      * @Route("/sso-request", name="sso_request")
+     * @param Request $request
+     * @return Response
      */
-    public function startRedirecting()
-    {// action
+    public function startRedirecting(Request $request): Response
+    {
+        if ($this->getUser() instanceof Account) {
+            return $this->redirectToRoute('complete');
+        }
+
+        $redirect = 'furtherRedirect=http://localhost:8080/sso-success';
+
+        $url = "http://".self::SSO_IP."/sso?".$redirect."&localSessionId=".$request->getClientIp();
+
+        return $this->redirect($url);
     }
 
     /**
      * Catch SSO success message
      *
-     * @Route("/sso-success/{token}", name="sso_receive")
+     * @Route("/sso-success", name="sso_receive")
      *
-     * @param string $token
+     * @param Request          $request
+     * @param SessionInterface $session
      * @return Response
      */
-    public function receive(string $token): Response
+    public function receive(Request $request, SessionInterface $session): Response
     {
-        return $this->render('received.html.twig', ['token' => $token]);
+        $client = HttpClient::create();
+        $token = $request->get('token');
+
+        try {
+            $options = [
+                'headers' => [
+                    'Authorization' => "Bearer {$token}",
+                ],
+            ];
+
+            $response = $client->request('GET', 'http://'.self::SSO_IP.'/api/account/current', $options);
+            $content = $response->getContent();
+        } catch (TransportExceptionInterface $e) {
+            dd($e);
+        } catch (ClientExceptionInterface $e) {
+            dd($e);
+        } catch (RedirectionExceptionInterface $e) {
+            dd($e);
+        } catch (ServerExceptionInterface $e) {
+            dd($e);
+        }
+        $account = $this->serializer->deserialize($content, Account::class, 'json');
+
+
+        $session->set('user', $account);
+
+
+        return $this->redirectToRoute('complete');
     }
 
     /**
-     * @Route("/success")
+     * @Route("/complete", name="complete")
+     * @param Request          $request
+     * @param SessionInterface $session
      * @return Response
      */
-    public function success()
+    public function showAuthedUser(Request $request, SessionInterface $session): Response
     {
-        return $this->render('success.html.twig');
+        return $this->render(
+            'received.html.twig',
+            [
+                'token' => $request->get('token'),
+                'user' => $session->get('user'),
+            ]
+        );
     }
 }
---
+++ src/Entity/Account.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+use function array_unique;
+
+class Account implements UserInterface
+{
+    private $id;
+    private $email;
+    private $roles = [];
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
